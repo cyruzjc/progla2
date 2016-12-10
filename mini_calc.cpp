db915d52084d8720654c2d8c5b528a5bb947c0bb
Joefrey Cyruz L. Alvarez <cyruz.alvarez@yahoo.com.ph>
2016-12-10 18:21:18 +0800

    Update mini_calc.cpp

--- mini_calc.cpp
+++ mini_calc.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-int get_Sum (int x,int y)
+int get_Sum (int x, int y)
 {
 	return x+y;
 }
-int get_Difference (int x,int y)
+int get_Difference (int x, int y)
 {
 	return x-y;
 }
-int get_Product (int x,int y)
+int get_Product (int x, int y)
 {
 	return x*y;
 }
-int get_Quotient (int x,int y)
+int get_Quotient (int x, int y)
 {
 	return x/y;
 }
@@ -22,16 +22,16 @@ int main ()
 {
 	int Num1,Num2;
 	
-	cout << "Enter Num 1: ";
+	cout << "Enter Number 1: ";
 	cin >> Num1;
 	
-	cout << "Enter Num 2: ";
+	cout << "Enter Number 2: ";
 	cin >> Num2;
 	
-	cout << "Sum is:"<<get_Sum(Num1,Num2) << endl;
-	cout << "Difference is:"<<get_Difference(Num1,Num2) << endl;
-	cout << "Product is:"<<get_Product(Num1,Num2) << endl;
-	cout << "Quotient is:"<<get_Quotient(Num1,Num2) << endl;
+	cout << "Sum is: "<<get_Sum(Num1,Num2) << endl;
+	cout << "Difference is: "<<get_Difference(Num1,Num2) << endl;
+	cout << "Product is: "<<get_Product(Num1,Num2) << endl;
+	cout << "Quotient is: "<<get_Quotient(Num1,Num2) << endl;
 	
 	return 0;
 }
